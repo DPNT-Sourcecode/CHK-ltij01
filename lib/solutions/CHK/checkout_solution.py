30f3b327b3a123647da3dae2d3a8fab496c4f99b
sbx_user1051 <sbx_user1051@169.254.7.53>
2024-02-27 15:37:50 +0000

    Tue Feb 27 15:37:50 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,17 @@
 # skus = unicode string
 def checkout(skus):
     prices = {"A": 50, "B": 30, "C":20, "D":15}
-    deals = {"A": {3: 130}, "B": {2:45}}
+    #deals = {"A": {3: 130}, "B": {2:45}}
+    deals =  {"3A": 130, "2B": 45}
+    total = 0
     for token in skus.split():
+        if token in prices: 
+            total+= prices[token]  
+        elif token in deals:
+            total+=deals[token]
+        else:
+            return -1
+    return total
 
-    raise NotImplementedError()
 
 
