12f1fb413072c7583b2aa934070d175cd9a09832
sbx_user1051 <sbx_user1051@169.254.7.53>
2024-02-27 16:05:59 +0000

    Tue Feb 27 16:05:59 UTC 2024

--- test/solution_tests/checkout_tests/test_checkout.py
+++ test/solution_tests/checkout_tests/test_checkout.py
@@ -1,10 +1,10 @@
-
+from solutions.CHK import checkout
 
 class TestCheckout(): 
     def test_checkout_singles(): 
-        ...
+        skus = "ABC"
     def test_checkout_invalid():
-        ...
+        skus = ""
     def test_checkout_deals(): 
         ... 
     def test_checkout_specials():
