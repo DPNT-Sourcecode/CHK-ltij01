729c75032af5b30b478aba748b2df5c8fae4fcee
sbx_user1051 <sbx_user1051@169.254.7.53>
2024-02-27 16:14:59 +0000

    Tue Feb 27 16:14:59 UTC 2024

--- test/solution_tests/checkout_tests/test_checkout.py
+++ test/solution_tests/checkout_tests/test_checkout.py
@@ -1,5 +1,43 @@
-from solutions.CHK import checkout
-import unnittest
+import unittest
+
+"""
+@param skus: unicode string, non space separated letters, no numbers
+"""
+def checkout(skus):
+    prices = {"A": 50, "B": 30, "C":20, "D":15, "E":40}
+    deals = {"A": [(5,200), (3, 130)], "B": (2, 45)}
+    specials = {"E": {"B": (2,1)}}
+    total_cost = 0
+    goods_purchased = {}
+    for sku in skus:
+        if sku not in prices: 
+            return -1 
+        if sku not in goods_purchased: 
+            goods_purchased[sku] = 1
+        else: 
+            goods_purchased[sku] +=1
+    for sku in goods_purchased: 
+        count = goods_purchased[sku]
+        if sku in specials: 
+            for freebie_sbu in specials[sku]: 
+                sku_count, freebie_count = specials[sku][freebie_sbu]
+                goods_purchased[freebie_sbu] -= (count // sku_count) * freebie_count
+                if goods_purchased[freebie_sbu] < 0: goods_purchased[freebie_sbu] = 0
+
+    for sku in goods_purchased: 
+        count = goods_purchased[sku]
+        if sku in deals: 
+            current_deal = 0 
+            deal_count, deal_price = deals[sku][current_deal]
+            while count >= deal_count: 
+                count -= deal_count
+                total_cost += goods_purchased
+                if count < deal_count:
+                    current_deal +=1 
+                    deal_count, deal_price = deals[sku][current_deal]
+
+        total_cost += count * prices[sku]
+    return total_cost
 
 class TestCheckout(unittest.TestCase): 
     def test_checkout_singles(): 
@@ -21,5 +59,6 @@ class TestCheckout(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
